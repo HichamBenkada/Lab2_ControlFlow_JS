a8b7a21b85104bed6bff8823a56c12170cef28a3
HichamBENKADA <hicham20014@gmail.com>
2023-11-23 15:47:53 -0500

    Part 2 Think bigger successfully implemented

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/**
- * ALAB 308.2.1 JavaScript: Control Flow:
+/** ALAB 308.2.1 JavaScript: Control Flow:
  This assignment is to develop a simple program that generates recommendations based on a set of input data and several conditions
 */
 /**
@@ -21,7 +20,7 @@ console.log(
 //Each plant requires a minimum space of 0.8 square meters.
 const minArea = 0.8;
 
-console.log('the minimum plant area is '+minArea+ ' square meter each.\n   ')
+console.log('the minimum plant area is '+minArea+ ' square meter each.\n ')
 
 // The area is starting with 20 plants.
 let plantCount = 20;
@@ -31,7 +30,7 @@ let weekNum = 0;
 console.log("  After " + weekNum + " week(s):");
 
 // Predicting the plant growth after a specific number of weeks.
-plantCount *= 2 ** weekNum;
+plantCount *= (2 ** weekNum);
 console.log(`The plants number in week ${weekNum} is ${plantCount} plants`);
 
 //  Implementing control flow to make decisions based on the plant count
@@ -62,7 +61,6 @@ if (plantPercentage > 80) { // if the growth exceeds 80% of the total capacity:
 /**
  * After one week 
  */
-
 weekNum++;
 console.log("  # After " + weekNum + " week(s):");
 // Predict the plant growth after a specific number of weeks as the plants double every week
@@ -80,16 +78,12 @@ console.log( "the percentage area covered by the " + plantCount + " plants is " 
 //Control Flow:
 if (plantPercentage > 80) { // if the growth exceeds 80% of the total capacity:
   console.log(
-    `The plants should be Pruned.\n 'The plants cover ${plantPercentage}% of the garden space which exceeds 80% of the maximum capacity'`
-  );
+    `The plants should be Pruned.\n 'The plants cover ${plantPercentage}% of the garden space which exceeds 80% of the maximum capacity'`);
 } else if (50 <= plantPercentage && plantPercentage <= 80){
   //if growth is betwwen 50 and 80%
-
   console.log(
     `The plants should be Monitored.\n 'The plants cover ${plantPercentage}% of the garden space (between 50 and 80%'` );
-
 } else { //The growth is bellow 50%
-
   console.log(`The plants should be "Planted".\n "The plants cover only ${plantPercentage}% of the garden space (Less than 50%)"`);
 }
 
@@ -101,7 +95,7 @@ console.log("  ## After " + weekNum + " week(s):");
 // Predict the plant growth after a specific number of weeks as the plants double every week
 plantCount *= 2 ** weekNum;
 console.log(`The plants number in week ${weekNum} is ${plantCount} plants`);
-
+//Area covered by the plant
 plantArea = minArea * plantCount;
 console.log(`The area covered by the plants is ${plantCount}*${minArea} = ${plantArea} square meters`);
 
@@ -113,18 +107,15 @@ console.log( "the percentage area covered by the " + plantCount + " plants is " 
 //Control Flow:
 if (plantPercentage > 80) { // if the growth exceeds 80% of the total capacity:
   console.log(
-    `The plants should be Pruned.\n 'The plants cover ${plantPercentage}% of the garden space which exceeds 80% of the maximum capacity'`
-  );
+    `The plants should be Pruned.\n 'The plants cover ${plantPercentage}% of the garden space which exceeds 80% of the maximum capacity'`);
 } else if (50 <= plantPercentage && plantPercentage <= 80){
   //if growth is betwwen 50 and 80%
-
   console.log(
     `The plants should be Monitored.\n 'The plants cover ${plantPercentage}% of the garden space (between 50 and 80%'` );
-
 } else { //The growth is bellow 50%
-
   console.log(`The plants should be "Planted".\n "The plants cover only ${plantPercentage}% of the garden space (Less than 50%)"`);
 }
+
 /**
  * After 3 weeks 
  */
@@ -134,6 +125,7 @@ console.log("  ### After " + weekNum + " week(s):");
 plantCount *= 2 ** weekNum;
 console.log(`The plants number in week ${weekNum} is ${plantCount} plants`);
 
+// Area of covered by the plants
 plantArea = minArea * plantCount;
 console.log(`The area covered by the plants is ${plantCount}*${minArea} = ${plantArea} square meters`);
 
@@ -141,19 +133,38 @@ console.log(`The area covered by the plants is ${plantCount}*${minArea} = ${plan
 plantPercentage = (plantArea / area) * 100;
 plantPercentage = plantPercentage.toFixed(2);
 console.log( "the percentage area covered by the " + plantCount + " plants is " + plantPercentage + "% of the garden area");
-
 //Control Flow:
 if (plantPercentage > 80) { // if the growth exceeds 80% of the total capacity:
   console.log(
-    `The plants should be Pruned.\n 'The plants cover ${plantPercentage}% of the garden space which exceeds 80% of the maximum capacity'`
-  );
+    `The plants should be Pruned.\n 'The plants cover ${plantPercentage}% of the garden space which exceeds 80% of the maximum capacity'`);
 } else if (50 <= plantPercentage && plantPercentage <= 80){
   //if growth is betwwen 50 and 80%
-
   console.log(
     `The plants should be Monitored.\n 'The plants cover ${plantPercentage}% of the garden space (between 50 and 80%'` );
-
 } else { //The growth is bellow 50%
-
   console.log(`The plants should be "Planted".\n "The plants cover only ${plantPercentage}% of the garden space (Less than 50%)"`);
 }
+
+/**
+ * Part 2: Thinking Bigger
+ */
+console.log("...  Part 2: Thinking Bigger  ...")
+// If the scientists were to start with 100 plants and not to be pruned for 10 weeks. What would be the additional space required given that there is multiple gardens with similar area.
+plantCount = 100;
+console.log('The Scientists decide to start with ' + plantCount + ' plants')
+weekNum = 10;
+//After 10 weeks the plants would double 10 times...
+plantCount = plantCount * (2**weekNum)
+console.log('After '+ weekNum +' weeks, the plants grow to '+ plantCount + ' plants.')
+// the plant area is: 0.8 * the count of plants
+plantArea = minArea * plantCount;
+console.log('This plants will cover a total area of ' +plantArea+ ' square metter.')
+
+radius= (plantArea/PI)**(.5)
+console.log(`If the space remained circular, the radius of the expanded garden will be: r = ${radius.toFixed(2)} meters.`)
+
+/**
+ * Part 3: Errors in Judgement
+ */
+console.log("...  Part 3: Errors in Judgement  ...")
+
