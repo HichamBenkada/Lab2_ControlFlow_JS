0a2131c6299e718beb8e3318bc296b79c98d77c0
HichamBENKADA <hicham20014@gmail.com>
2023-11-23 14:55:25 -0500

    The growth control system is updated to include plants growth after 1,2 and 3 weeks

--- script.js
+++ script.js
@@ -14,40 +14,146 @@ let radius = 5;
 const PI = 3.1415;
 const area = PI * radius * radius;
 
-console.log('The plants are contained in circular area of '+ area.toFixed(4) + ' square meters')
+console.log(
+  "The plants are to be contained in a garden of a circular area of " + area.toFixed(4) + " square meters"
+);
 
 //Each plant requires a minimum space of 0.8 square meters.
-let plantArea = 0.8;
+const minArea = 0.8;
+
+console.log('the minimum plant area is '+minArea+ ' square meter each.\n   ')
+
 // The area is starting with 20 plants.
-let plantCount = 20; 
-//The plants double in number every week
-let weekNum = 2;
-plantCount *= (2 ** weekNum);
+let plantCount = 20;
 
-// Predict the plant growth after a specific number of weeks.
+//The plants "double" in number every week
+let weekNum = 0;
+console.log("  After " + weekNum + " week(s):");
 
+// Predicting the plant growth after a specific number of weeks.
+plantCount *= 2 ** weekNum;
 console.log(`The plants number in week ${weekNum} is ${plantCount} plants`);
 
 //  Implementing control flow to make decisions based on the plant count
+let plantArea = minArea * plantCount;
+console.log(`The area covered by the plants is ${plantCount}*${minArea} = ${plantArea} square meters`);
 
-plantArea = plantArea * plantCount;
+//the percentage area covered by the plants
+let plantPercentage = (plantArea / area) * 100;
+plantPercentage = plantPercentage.toFixed(2);
+console.log( "the percentage area covered by the " + plantCount + " plants is " + plantPercentage + "% of the garden area");
 
-console.log(`The area covered by ${plantCount} plants is ${plantArea} square meters`)
+//Control Flow:
+if (plantPercentage > 80) { // if the growth exceeds 80% of the total capacity:
+  console.log(
+    `The plants should be Pruned.\n 'The plants cover ${plantPercentage}% of the garden space which exceeds 80% of the maximum capacity'`
+  );
+} else if (50 <= plantPercentage && plantPercentage <= 80){
+  //if growth is betwwen 50 and 80%
 
-let plantPercentage = plantArea/area *100;
-plantPercentage = plantPercentage.toFixed(2)
-console.log('the percentage area covered by the '+ plantCount + ' is ' + plantPercentage + '%.' )
+  console.log(
+    `The plants should be Monitored.\n 'The plants cover ${plantPercentage}% of the garden space (between 50 and 80%'` );
 
-// if the plants growth after the given number of weeks exceed 80% of the maximum capacity of the garden, the plants should be Pruned.
-if (plantPercentage > 80){
+} else { //The growth is bellow 50%
 
-    console.log(`The plants should be Pruned, because the plants cover ${plantPercentage}% of the garden space which exceeds 80% of the maximum capacity`)
-
-} else if (50 <= plantPercentage <= 80){ //if growth betwwen 50 and 80%
-
-    console.log(`The plants should be Pruned, because the plants cover ${plantPercentage}% of the garden space which exceeds 80% of the maximum capacity`) 
-
-} else{
-
+  console.log(`The plants should be "Planted".\n "The plants cover only ${plantPercentage}% of the garden space (Less than 50%)"`);
 }
 
+/**
+ * After one week 
+ */
+
+weekNum++;
+console.log("  # After " + weekNum + " week(s):");
+// Predict the plant growth after a specific number of weeks as the plants double every week
+plantCount *= 2 ** weekNum;
+console.log(`The plants number in week ${weekNum} is ${plantCount} plants`);
+
+plantArea = minArea * plantCount;
+console.log(`The area covered by the plants is ${plantCount}*${minArea} = ${plantArea} square meters`);
+
+//the percentage area covered by the plants
+plantPercentage = (plantArea / area) * 100;
+plantPercentage = plantPercentage.toFixed(2);
+console.log( "the percentage area covered by the " + plantCount + " plants is " + plantPercentage + "% of the garden area");
+
+//Control Flow:
+if (plantPercentage > 80) { // if the growth exceeds 80% of the total capacity:
+  console.log(
+    `The plants should be Pruned.\n 'The plants cover ${plantPercentage}% of the garden space which exceeds 80% of the maximum capacity'`
+  );
+} else if (50 <= plantPercentage && plantPercentage <= 80){
+  //if growth is betwwen 50 and 80%
+
+  console.log(
+    `The plants should be Monitored.\n 'The plants cover ${plantPercentage}% of the garden space (between 50 and 80%'` );
+
+} else { //The growth is bellow 50%
+
+  console.log(`The plants should be "Planted".\n "The plants cover only ${plantPercentage}% of the garden space (Less than 50%)"`);
+}
+
+/**
+ * After 2 weeks 
+ */
+weekNum++;
+console.log("  ## After " + weekNum + " week(s):");
+// Predict the plant growth after a specific number of weeks as the plants double every week
+plantCount *= 2 ** weekNum;
+console.log(`The plants number in week ${weekNum} is ${plantCount} plants`);
+
+plantArea = minArea * plantCount;
+console.log(`The area covered by the plants is ${plantCount}*${minArea} = ${plantArea} square meters`);
+
+//the percentage area covered by the plants
+plantPercentage = (plantArea / area) * 100;
+plantPercentage = plantPercentage.toFixed(2);
+console.log( "the percentage area covered by the " + plantCount + " plants is " + plantPercentage + "% of the garden area");
+
+//Control Flow:
+if (plantPercentage > 80) { // if the growth exceeds 80% of the total capacity:
+  console.log(
+    `The plants should be Pruned.\n 'The plants cover ${plantPercentage}% of the garden space which exceeds 80% of the maximum capacity'`
+  );
+} else if (50 <= plantPercentage && plantPercentage <= 80){
+  //if growth is betwwen 50 and 80%
+
+  console.log(
+    `The plants should be Monitored.\n 'The plants cover ${plantPercentage}% of the garden space (between 50 and 80%'` );
+
+} else { //The growth is bellow 50%
+
+  console.log(`The plants should be "Planted".\n "The plants cover only ${plantPercentage}% of the garden space (Less than 50%)"`);
+}
+/**
+ * After 3 weeks 
+ */
+weekNum++;
+console.log("  ### After " + weekNum + " week(s):");
+// Predict the plant growth after a specific number of weeks as the plants double every week
+plantCount *= 2 ** weekNum;
+console.log(`The plants number in week ${weekNum} is ${plantCount} plants`);
+
+plantArea = minArea * plantCount;
+console.log(`The area covered by the plants is ${plantCount}*${minArea} = ${plantArea} square meters`);
+
+//the percentage area covered by the plants
+plantPercentage = (plantArea / area) * 100;
+plantPercentage = plantPercentage.toFixed(2);
+console.log( "the percentage area covered by the " + plantCount + " plants is " + plantPercentage + "% of the garden area");
+
+//Control Flow:
+if (plantPercentage > 80) { // if the growth exceeds 80% of the total capacity:
+  console.log(
+    `The plants should be Pruned.\n 'The plants cover ${plantPercentage}% of the garden space which exceeds 80% of the maximum capacity'`
+  );
+} else if (50 <= plantPercentage && plantPercentage <= 80){
+  //if growth is betwwen 50 and 80%
+
+  console.log(
+    `The plants should be Monitored.\n 'The plants cover ${plantPercentage}% of the garden space (between 50 and 80%'` );
+
+} else { //The growth is bellow 50%
+
+  console.log(`The plants should be "Planted".\n "The plants cover only ${plantPercentage}% of the garden space (Less than 50%)"`);
+}
